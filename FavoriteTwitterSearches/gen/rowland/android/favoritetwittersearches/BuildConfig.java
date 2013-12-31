892bd02ec8ff75db537d5f457fc925a640ed98c6
Joseph Rowland <naugeblue@yahoo.com>
2013-12-31 17:09:32 -0500

    Init

---
+++ FavoriteTwitterSearches/gen/rowland/android/favoritetwittersearches/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package rowland.android.favoritetwittersearches;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
